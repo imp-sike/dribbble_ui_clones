7f4646c86e42c73fd2f1f28d60f47a35a0492391
imp-sike <sulavparajuli07@gmail.com>
2022-07-08 06:10:48 +0545

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:smooth_ui_example/ui/ui_01.dart';
+
+void main() => runApp(
+  const MaterialApp(
+    home: Ui01(),
+  )
+);
---
+++ lib/ui/ui_01.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Ui01 extends StatelessWidget {
+  const Ui01({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text("Hello"),
+    );   
+  }
+}
